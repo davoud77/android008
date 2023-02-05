afddcdbb7de96b617e7d174452858c5d0e1f4a40
davoud77 <davoud.fathi1377@gmail.com>
2023-02-05 18:17:41 +0330

    add class to main

--- app/src/main/java/com/example/git008/MainActivity.kt
+++ app/src/main/java/com/example/git008/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.git008
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.git008.utils.Constans
 
 class MainActivity : AppCompatActivity() {
+
+    private val constant: Constans by lazy { Constans(this) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -12,6 +15,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun main() {
-
+        val classMe = constant
     }
 }
--- app/src/main/java/com/example/git008/utils/Constans.kt
+++ app/src/main/java/com/example/git008/utils/Constans.kt
@@ -1,4 +1,15 @@
 package com.example.git008.utils
 
-class Constans {
+import android.content.Context
+import android.widget.Toast
+
+class Constans(context: Context) {
+
+    private val name = "davoud"
+    private val age = 15000
+    private val family = "fathi"
+
+    init {
+        Toast.makeText(context, "$name and $age and $family", Toast.LENGTH_SHORT).show()
+    }
 }
